8791b116654a99b31aa272538dee3fa30747848b
Alexander Morogov <asmorogov@gmail.com>
2019-12-11 16:48:36 +0200

    Settings fix

--- src/com/test/Settings.groovy
+++ src/com/test/Settings.groovy
@@ -3,25 +3,34 @@ package com.test
 import groovy.json.JsonSlurperClassic
 
 class Settings implements Serializable{
-    public Object _settings
+    private HashMap _settings
     private String _branch
     private String _project
-    Settings(String json){
+    def _context
+    Settings(String json, context){
         _settings = new JsonSlurperClassic().parseText(json)
+        _context = context
     }
     String getAt(String item){
+        _context.echo "Settings.getAt: ${item}"
         if(_project == null)
             throw new Exception("Settings error: Project name is not set")
         if(_branch == null)
             throw new Exception("Settings error: Branch name is not set")
-        if(!_settings.containsKey(item))
+        if(!_settings.containsKey(item)){
+            _context.echo "Settings.getAt: not contains ${item}"
             return ''
-        return _settings[_project][_branch][item] as String
+        }
+        def result = _settings[_project][_branch][item] as String
+        _context.echo "Settings.getAt: result is: ${result}"
+        return result
     }
     def setProject(String project){
+        _context.echo "Settings.setProject: ${project}"
         _project = project
     }
     def setBranch(String branch){
+        _context.echo "Settings.setBranch: ${branch}"
         _branch = branch
     }
     String[] getProjects(){
