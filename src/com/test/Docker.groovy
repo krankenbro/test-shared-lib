e3bc1d39039624d59d4c3ba0991a2924a5a58ab0
Alexander Morogov <asmorogov@gmail.com>
2019-12-17 10:28:09 +0200

    Docker class

--- src/com/test/Docker.groovy
+++ src/com/test/Docker.groovy
@@ -24,8 +24,8 @@ class Docker implements Serializable{
     }
 
     def static build(context, contextFolder, imageName, args){
-        context.dir(contextFolder){
+        //context.dir(contextFolder){
             return context.docker.build(imageName, args)
-        }
+        //}
     }
 }
