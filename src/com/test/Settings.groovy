06fa3c60a49a460e78ac91f49f3909df53439e7d
Alexander Morogov <asmorogov@gmail.com>
2019-12-18 09:32:23 +0200

    Settings: Cannot invoke method containsKey() on null object fix

--- src/com/test/Settings.groovy
+++ src/com/test/Settings.groovy
@@ -14,7 +14,7 @@ class Settings implements Serializable{
             throw new Exception("Settings error: Project name is not set")
         if(_branch == null)
             throw new Exception("Settings error: Branch name is not set")
-        if(!_settings[_project][_branch].containsKey(item)){
+        if(!_settings[_project][_branch]?.containsKey(item)){
             return ''
         }
         def result = _settings[_project][_branch][item] as String
