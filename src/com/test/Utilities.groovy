5020d2f737331d291820674616ebe14507831f39
Alexander Morogov <asmorogov@gmail.com>
2020-03-30 19:46:30 +0200

    runPS pwsh

--- src/com/test/Utilities.groovy
+++ src/com/test/Utilities.groovy
@@ -37,7 +37,7 @@ class Utilities {
     }
     def static runPS(context, scriptName, args = '')
     {
-        context.powershell script: "${context.env.WORKSPACE}\\..\\workspace@libs\\${DefaultSharedLibName}\\resources\\${scriptName} ${args} -ErrorAction Stop", label: scriptName
+        context.pwsh script: "${context.env.WORKSPACE}\\..\\workspace@libs\\${DefaultSharedLibName}\\resources\\${scriptName} ${args} -ErrorAction Stop", label: scriptName
     }
     def static setSharedLibName(name){
         DefaultSharedLibName = name
