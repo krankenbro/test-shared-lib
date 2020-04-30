2f7c271771cbc4866c392fdcd70c6933eba43958
Alexander Morogov <asmorogov@gmail.com>
2020-04-30 11:15:58 +0200

    createInfrastructure

--- src/com/test/Utilities.groovy
+++ src/com/test/Utilities.groovy
@@ -664,4 +664,39 @@ class Utilities {
         context.echo changes.getClass() as String
         return changes.last().last().msg
     }
+    def static createInfrastructure(context, project = 'blank')
+    {
+        //def AzureTempDir = Utilities.getAzureTemplateDir(context)
+        def AzureTempDir = "..\\workspace@libs\\virto-shared-library\\resources\\azure\\arm-templates" // Azure Resource Manager templates path
+        context.dir(AzureTempDir)
+                {
+                    switch(project)
+                    {
+                        case 'bulk-update/dev':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructureBulkUpdateDev.ps1")
+                            break
+                        case 'bulk-update/master':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructureBulkUpdateQA.ps1")
+                            break
+                        case 'JS':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructureJsShoppingCartIntegrationSample.ps1")
+                            break
+                        case 'DEV-VC':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructureDEV-VC.ps1")
+                            break
+                        case 'PROD-VC':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructurePROD-VC.ps1")
+                            break
+                        case 'DEV-demoVC':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructureDEV-demoVC.ps1")
+                            break
+                        case 'QA-demoVC':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructureQA-demoVC.ps1")
+                            break
+                        case 'PROD-demoVC':
+                            Utilities.runSharedPS(context, "arm-templates//vc-CreateInfrastructurePROD-demoVC.ps1")
+                            break
+                    }
+                }
+    }
 }
