826050247ccd3fcc6917c898f7c79009f65bb979
Alexander Morogov <asmorogov@gmail.com>
2020-05-07 16:50:51 +0200

    tag_name -> name

--- src/com/test/GithubRelease.groovy
+++ src/com/test/GithubRelease.groovy
@@ -42,7 +42,7 @@ class GithubRelease{
         def releases = new groovy.json.JsonSlurperClassic().parseText(content)
         for(release in releases){
             context.echo release.tag_name
-            if(isMatch(release.tag_name, regexp) && release.prerelease == prerelease){
+            if(isMatch(release.name, regexp) && release.prerelease == prerelease){
                 context.echo "Release id: ${release.id}"
                 return new GithubRelease(release)
             }
