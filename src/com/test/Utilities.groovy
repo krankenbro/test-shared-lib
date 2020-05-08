2eafc39c2775962afef20588273000384801ac04
Alexander Morogov <asmorogov@gmail.com>
2020-05-08 16:39:15 +0200

    fix

--- src/com/test/Utilities.groovy
+++ src/com/test/Utilities.groovy
@@ -689,7 +689,7 @@ class Utilities {
     }
     def static getReleaseNotesFromCommits(context, since)
     {
-        def gitOut = context.pwsh(script: "@git log --pretty=format:\"%s\" --since=\"${since}\"", returnStdout: true).trim()
+        def gitOut = context.pwsh(script: "git log --pretty=format:\"%s\" --since=\"${since}\"", returnStdout: true).trim()
         context.echo "git output: ${gitOut}"
         return cleanReleaseNotes(context, gitOut).replaceAll("\n", "<br />")
     }
