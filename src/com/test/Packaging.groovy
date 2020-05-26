5ba8691ed63005ec2035aebe23a00eeabc38967f
Alexander Morogov <asmorogov@gmail.com>
2020-05-26 10:23:02 +0200

    fix

--- src/com/test/Packaging.groovy
+++ src/com/test/Packaging.groovy
@@ -40,7 +40,7 @@ class Packaging {
         context.echo "Building docker image \"${dockerImageName}\" using \"${dockerContextFolder}\" as context folder"
         context.bat "xcopy \"..\\workspace@libs\\virto-shared-library\\resources\\${dockerFolder}\\${dockerFileFolder}\\*\" \"${dockerContextFolder}\\\" /Y /E"
         def dockerImage
-        def runtimeImageArg = runtimeImage == "" ? "" : "RUNTIME_IMAGE=\"${runtimeImage}\""
+        def runtimeImageArg = runtimeImage == "" ? "" : "--build-arg RUNTIME_IMAGE=\"${runtimeImage}\""
         context.dir(dockerContextFolder)
         {
             dockerImage = context.docker.build("${dockerImageName}:${version}".toLowerCase(), "--build-arg SOURCE=\"${dockerSourcePath}\" ${runtimeImageArg} .")
